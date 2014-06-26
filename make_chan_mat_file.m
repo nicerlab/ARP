43e3916cbc536ad50f9429061b2aa1648367a4b9
NicerLab <nicerlab@gmail.com>
2014-06-26 12:27:44 -0400

    Prep for new code

--- make_chan_mat_file.m
+++
@@ -1,5 +0,0 @@
-CEDfile=input(['Please enter the BODY of the .ced file name to be converted to .mat (i.e. 70channel)'], 's');
-chanlocs=readlocs([CEDfile '.ced']);
-save([CEDfile '.ced'], 'chanlocs');
-formatSpec = '%s.ced converted to %s.mat';
-fprintf(formatSpec,CEDfile,CEDfile);  
